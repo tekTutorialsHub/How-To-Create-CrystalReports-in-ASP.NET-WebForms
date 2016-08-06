ad8c9a330bfb0706e3d59fadb6fbfcd755ba3165
tekTuturialsHub <admin@tekTutorialsHub.com>
2016-08-06 09:36:45 +0530

    Add project files.

---
+++ CrWebFormsApp/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CrWebFormsApp.Default" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Show" runat="server" OnClick="Show_Click" Text="Show" />
+    
+    </div>
+        <CR:CrystalReportViewer ID="CrystalReportViewer" runat="server" AutoDataBind="true" />
+    </form>
+</body>
+</html>
---
+++ CrWebFormsApp/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+namespace CrWebFormsApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        rptCustomerList rpt;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (Session["report"] != null)
+            {
+                CrystalReportViewer.ReportSource = (rptCustomerList)Session["report"];
+
+            }
+        }
+
+        protected void Show_Click(object sender, EventArgs e)
+        {
+            if (Session["report"] == null)
+            {
+                CustomerTableAdapters.CustomersTableAdapter da = new CustomerTableAdapters.CustomersTableAdapter();
+                
+                Customer ds = new Customer();
+                Customer.CustomersDataTable dt=(Customer.CustomersDataTable) ds.Tables["Customers"];
+                da.Fill(dt);
+                
+                rpt = new rptCustomerList();
+                rpt.SetDataSource(ds);
+                CrystalReportViewer.ReportSource = rpt;
+
+
+                Session.Add("report", rpt);
+            }
+
+
+        }
+
+    }
+}
